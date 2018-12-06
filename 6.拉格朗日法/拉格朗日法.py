e592fa6d2482e7b2ccca83ea5e44e9c914f0dcb3
JanuaryZeng <15841331413@163.com>
2018-12-06 21:46:27 +0800

    修改拉格朗日和牛顿插值法

--- 6.拉格朗日法/拉格朗日法.py
+++ 6.拉格朗日法/拉格朗日法.py
@@ -22,7 +22,6 @@ def Lagrangian(x):
     
 global xList
 xList = list()
-print('曾祥玖 1607094240')
 
 n = int(input('请输入点的个数:'))
 for i in range(n):
@@ -32,17 +31,23 @@ for i in range(n):
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 plt.rcParams['axes.unicode_minus'] = False
 line = np.arange(-3,3.5,0.1)
-plt.figure(figsize=(8,12))
-plt.subplot(2,1,1)
-plt.plot(line, func(line), color = 'b')
+plt.figure(figsize=(8,18))
+plt.subplot(3,1,1)
+plt.plot(line, func(line), color = 'g')
 plt.scatter(np.array(xList),func(np.array(xList)),color = 'r')
 plt.grid()
 plt.title('原函数和点')
-plt.subplot(2,1,2)
+plt.subplot(3,1,2)
 plt.plot(line, Lagrangian(line), color = 'b')
 plt.scatter(np.array(xList),func(np.array(xList)),color = 'r')
 plt.grid()
 plt.title('拟合函数')
+plt.subplot(3,1,3)
+plt.plot(line, Lagrangian(line), color = 'b')
+plt.plot(line, func(line), color = 'g')
+plt.scatter(np.array(xList),func(np.array(xList)),color = 'r')
+plt.grid()
+plt.title('两曲线重合度')
 '''
 最好效果为6个点！
 '''
